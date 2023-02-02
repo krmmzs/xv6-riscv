e2591b83ac17d871fadfa7eafe41ee6faf2c90bc
krmmzs <belliyan250@gmail.com>
2023-02-02 23:11:26 +0800

    Explain the data structure of the memory allocator itself and the corresponding operations

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -1,6 +1,13 @@
 // Physical memory allocator, for user processes,
 // kernel stacks, page-table pages,
 // and pipe buffers. Allocates whole 4096-byte pages.
+// -----------------------------------------------------
+// The implementation of allocator is a nice example.
+// The allocator sometimes treats addresses as integers in order to perform arithmetic on them
+// (e.g., traversing all pages infreerange), and sometimes uses addresses as pointers to read and
+// write memory (e.g., manipulating therunstructure stored in each page); this dual use of addresses
+// is the main reason that the allocator code is full of C type casts. The other reason is that freeing
+// and allocation inherently change the type of the memory.
 
 #include "types.h"
 #include "param.h"
@@ -14,15 +21,22 @@ void freerange(void *pa_start, void *pa_end);
 extern char end[]; // first address after kernel.
                    // defined by kernel.ld.
 
+// Act a node in the data structure
+// The data structure is a linked list
+// Each node is a page of memory
+// (content is alloced by memset, so without content field)
 struct run {
     struct run *next;
 };
 
+// Act as a head node in the data structure
 struct {
     struct spinlock lock;
     struct run *freelist;
 } kmem;
 
+// kinitinitializesthe free list to hold every page
+// between the end of the kernel(end[]) and PHYSTOP
 void
 kinit()
 {
@@ -43,6 +57,8 @@ freerange(void *pa_start, void *pa_end)
 // which normally should have been returned by a
 // call to kalloc().  (The exception is when
 // initializing the allocator; see kinit above.)
+// ---------------------------------------------
+// Head insertion method in the linked list
 void
 kfree(void *pa)
 {
@@ -52,6 +68,7 @@ kfree(void *pa)
         panic("kfree");
 
     // Fill with junk to catch dangling refs.
+    // hopefully that will cause such code to break faster
     memset(pa, 1, PGSIZE);
 
     r = (struct run*)pa;
@@ -65,6 +82,8 @@ kfree(void *pa)
 // Allocate one 4096-byte page of physical memory.
 // Returns a pointer that the kernel can use.
 // Returns 0 if the memory cannot be allocated.
+// ---------------------------------------------
+// Head removal method in the linked list
 void *
 kalloc(void)
 {
